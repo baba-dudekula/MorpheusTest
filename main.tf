067567a814b3a9ad1aefbb6df522f717acc9f6ae
baba-dudekula <babad0786@gmail.com>
2025-10-31 15:16:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
+
+# Configure the Azure provider using Service Principal credentials
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
 # Create a Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
